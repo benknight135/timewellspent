170caf7e7bf734ea1f71ded2612e4efff788fbc1
Ben Knight <benknight135@gmail.com>
2021-06-27 17:46:37 +0100

    fix update notification

--- js/main.js
+++ js/main.js
@@ -3,45 +3,43 @@ window.onload = () => {
 
   let newWorker;
 
-  // The click event on the notification
+  function showUpdateBar() {
+    let snackbar = document.getElementById('snackbar');
+    snackbar.className = 'show';
+  }
+
+  // The click event on the pop up notification
   document.getElementById('reload').addEventListener('click', function(){
     newWorker.postMessage({ action: 'skipWaiting' });
   });
 
   if ('serviceWorker' in navigator) {
-    // Register the service worker
-    navigator.serviceWorker.register('/sw.js').then(reg => {
-      reg.addEventListener('updatefound', () => {
 
-        // An updated service worker has appeared in reg.installing!
+    navigator.serviceWorker.register('/service-worker.js').then(reg => {
+      reg.addEventListener('updatefound', () => {
+        // A wild service worker has appeared in reg.installing!
         newWorker = reg.installing;
 
         newWorker.addEventListener('statechange', () => {
-
-          // Has service worker state changed?
+          // Has network.state changed?
           switch (newWorker.state) {
             case 'installed':
-
-	            // There is a new service worker available, show the notification
               if (navigator.serviceWorker.controller) {
-                let notification = document.getElementById('notification ');
-                notification .className = 'show';
+                // new update available
+                showUpdateBar();
               }
-
+              // No update available
               break;
           }
         });
       });
     });
 
+    let refreshing;
+    navigator.serviceWorker.addEventListener('controllerchange', function () {
+      if (refreshing) return;
+      window.location.reload();
+      refreshing = true;
+    });
   }
-
-  let refreshing;
-  // The event listener that is fired when the service worker updates
-  // Here we reload the page
-  navigator.serviceWorker.addEventListener('controllerchange', function () {
-    if (refreshing) return;
-    window.location.reload();
-    refreshing = true;
-  });
 }
