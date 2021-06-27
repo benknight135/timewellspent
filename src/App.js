60516a371c5f1be97c0f8e3ca552d66dd2f4177f
Ben Knight <benknight135@gmail.com>
2021-06-27 19:25:24 +0100

    add workflow build

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 class="title">Time Well Spent</h1>
+        <h1 className="title">Time Well Spent</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <h2 class="title">Coming soon!</h2>
+        <h2 className="title">Coming soon!</h2>
         <p><a href="mailto:support@timewellspent.app">support@timewellspent.app</a></p>
         <p>Last update: 18:14 27/06/2021</p>
       </header>
