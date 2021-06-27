88bab1812de8d7b433ebb9848b9cc83d1d1ac35a
Ben Knight <benknight135@gmail.com>
2021-06-27 19:02:36 +0100

    change to react pwa

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 class="title">Time Well Spent</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2 class="title">Coming soon!</h2>
+        <p><a href="mailto:support@timewellspent.app">support@timewellspent.app</a></p>
+        <p>Last update: 18:14 27/06/2021</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
