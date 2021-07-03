0de8611a1c8c0c71708efd4923f61e4e0755c796
Ben <benknight135@gmail.com>
2021-07-03 18:59:02 +0100

    move meta tag control to component

--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -1,10 +1,15 @@
 import logo from './../data/logo.svg';
+import MetaTags from 'react-meta-tags';
 import './ComingSoon.css';
 
 function ComingSoon() {
     const { REACT_APP_LAST_UPDATE } = process.env;
     return (
         <div className="ComingSoon">
+            <MetaTags>
+                <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
+            </MetaTags>
             <header className="ComingSoon-header">
                 <h1 className="title">Time Well Spent</h1>
                 <img src={logo} className="ComingSoon-logo" alt="logo" />
