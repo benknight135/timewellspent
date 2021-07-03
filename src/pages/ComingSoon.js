d42f524f1406b583d93bd758fc9ed2c887345e93
Ben <benknight135@gmail.com>
2021-07-03 17:59:22 +0100

    remove email from page

--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -9,7 +9,6 @@ function ComingSoon() {
                 <h1 className="title">Time Well Spent</h1>
                 <img src={logo} className="ComingSoon-logo" alt="logo" />
                 <h2 className="title">Coming soon!</h2>
-                <p><a href="mailto:support@timewellspent.app">support@timewellspent.app</a></p>
                 <p>Last update: {REACT_APP_LAST_UPDATE}</p>
             </header>
         </div>
