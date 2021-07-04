c9d0bb7670c835f9f3ea9712068b815ddcdf9c77
Ben <benknight135@gmail.com>
2021-07-04 03:14:16 +0100

    try custom vh prop

--- src/games/TicTac.js
+++ src/games/TicTac.js
@@ -153,6 +153,9 @@ class TicTac extends React.Component {
       historyOrderText = "Asce";
     }
 
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
     return (
       <div className="tictac">
         <div className="tictac-board">
